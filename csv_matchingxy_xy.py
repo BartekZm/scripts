a12b7aa354a28e0766336ecc021fea79fa34cce0
BartekZm <78498132+BartekZm@users.noreply.github.com>
2024-08-26 13:05:31 +0200

    Create csv_matchingxy_xy.py

---
+++ csv_matchingxy_xy.py
@@ -0,0 +1,17 @@
+import pandas
+
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.width', None)
+pandas.set_option('display.max_colwidth', None)
+
+#data format X,Y,Z
+df = pandas.read_csv("<OUTPUT_FILE_PATH>")
+
+for i, row in df.iterrows():
+    match=df[(df['Y'] == row['Z']) & (df['Z'] == row['Y'])]
+
+    if not match.empty:
+        df.at[i,'A'] = str(match['X'].values[0]).rstrip(".0")
+
+print(df)
